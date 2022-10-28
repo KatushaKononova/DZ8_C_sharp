23407b0876414bf8f5941a12e1fb09c752e2a4c8
KatushaKononova56 <83833827+KatushaKononova56@users.noreply.github.com>
2022-10-28 21:45:46 +0300

    add task1

---
+++ test1/Program.cs
@@ -0,0 +1,8 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num>99 && num<1000){
+    int second_num = (num/10)%10;
+    Console.WriteLine($"Для числа {num} второе число {second_num}");
+}
+else Console.WriteLine("Число не трехначное");
